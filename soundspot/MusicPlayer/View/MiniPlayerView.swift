884fc68b5ccf64b138aa69ea754b67413a7cca7a
James <jamesmichael.maturino.46@my.csun.edu>
2022-04-26 04:08:23 -0700

    Functionality for miniPlayer written, code to show on homeView not implemented

--- soundspot/MusicPlayer/View/MiniPlayerView.swift
+++ soundspot/MusicPlayer/View/MiniPlayerView.swift
@@ -10,15 +10,81 @@ import SwiftUI
 
 struct MiniPlayerView: View
 {
+    @StateObject var viewModel : PlayerViewModel
+    
     var body: some View
     {
-        VStack
+        HStack
         {
+            Spacer()
+            //PlayerView...
+            if(viewModel.trackList[viewModel.trackIndex].pictureData == nil && !viewModel.trackList[viewModel.trackIndex].pictureDownloaded){
+                Image("defaultTrackImg")
+                    .resizable()
+                    .frame(width: 70, height: 70)
+            }else{
+                if let data = viewModel.trackList[viewModel.trackIndex].pictureData{
+                    if let uiImage = UIImage(data: data){
+                        Image(uiImage: uiImage)
+                            .resizable()
+                            .frame(width: 70, height: 70)
+                    }
+                }
+            }
             
+            VStack(alignment: .leading, spacing: 6, content:
+                    {
+                Text(viewModel.trackList[viewModel.trackIndex].title)
+                    .font(.callout)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .lineLimit(1)
+                
+                /*Text("Song Artist")
+                    //.fontWeight(.bold)
+                    .font(.caption)
+                    .foregroundColor(.black)*/
+                })
+            
+            Spacer()
+            
+            Button(action: {viewModel.onEvent(event: MusicPlayerEvent.PreviousPressed)}, label: {
+                Image(systemName: "backward.end.fill")
+                    .font(.title2)
+                    .foregroundColor(.black)
+            })
+            Spacer()
+            
+            Button(action: {viewModel.onEvent(event: MusicPlayerEvent.PlayPausePressed)},
+                   label: {
+                Image(systemName: viewModel.isPlaying ? "pause.fill" : "play.fill")
+                    .font(.title2)
+                    .foregroundColor(.black)
+            })
+            Spacer()
+            Button(action: {viewModel.onEvent(event: MusicPlayerEvent.NextPressed)},
+                   label: {
+                Image(systemName: "forward.end.fill")
+                    .font(.title2)
+                    .foregroundColor(.black)
+            })
+            /*
+            Button(action: {}, label: {
+                Image(systemName: "xmark")
+                    .font(.title2)
+                    .foregroundColor(.black)
+            })*/
+            Spacer()
         }
+        .padding(.trailing)
+        .background(Color.gray)
+        .frame(maxWidth: .infinity)
     }
+
 }
 
+
+//Delete this code after properly implementing the MiniPlayerView
 struct VideoControls: View
 {
     //@EnvironmentObject var player: PlayerView
@@ -26,50 +92,57 @@ struct VideoControls: View
     {
         HStack
         {
+            Spacer()
             //PlayerView...
-            Rectangle()
-                .fill(Color.white)
-                .frame(width: 150, height: 70)
+            Image("defaultTrackImg")
+                .resizable()
+                .frame(width: 70, height: 70)
             
             VStack(alignment: .leading, spacing: 6, content:
                     {
                 Text("Song Title")
                     .font(.callout)
-                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
                     .lineLimit(1)
                 
                 Text("Song Artist")
-                    .fontWeight(.bold)
+                    //.fontWeight(.bold)
                     .font(.caption)
-                    .foregroundColor(.gray)
+                    .foregroundColor(.black)
                 })
             
+            Spacer()
             
             Button(action: {}, label: {
                 Image(systemName: "backward.end.fill")
                     .font(.title2)
-                    .foregroundColor(.white)
+                    .foregroundColor(.black)
             })
+            Spacer()
             
             Button(action: {}, label: {
                 Image(systemName: "pause.fill")
                     .font(.title2)
-                    .foregroundColor(.white)
+                    .foregroundColor(.black)
             })
-            
+            Spacer()
             Button(action: {}, label: {
                 Image(systemName: "forward.end.fill")
                     .font(.title2)
-                    .foregroundColor(.white)
+                    .foregroundColor(.black)
             })
-            
+            /*
             Button(action: {}, label: {
                 Image(systemName: "xmark")
                     .font(.title2)
-                    .foregroundColor(.white)
-            })
+                    .foregroundColor(.black)
+            })*/
+            Spacer()
         }
         .padding(.trailing)
+        .background(Color.gray)
+        .frame(maxWidth: .infinity)
     }
 }
 
